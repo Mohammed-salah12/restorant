c1205f66c1f3db460aef19e38a2e0e785f656344
JumanaAlkfarna <jumanawael4@gmail.com>
2023-02-09 15:25:13 +0200

    E_commit

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -11,7 +11,5 @@ class Like extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
-    public function likes(){
-        return $this->hasMany(Like::class);
-    }
+    
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
         return $this->morphTo();
     }
 
+    public function likes(){
+        return $this->hasMany(Like::class);
+    }
+
 }
